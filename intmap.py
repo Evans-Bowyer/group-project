df4ae28ee6d7e61edd3f0fa848256a9d54da6322
Andrew Goetz <andrewmigoetz@gmail.com>
2023-04-04 13:25:49 -0500

    Added interactive map

--- intmap.py
+++ intmap.py
@@ -1,31 +1,16 @@
-import os
-import pandas as pd
-import geopandas as gpd
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import xarray as xr
 
-#hello
-# csv_files = []
-# def get_csv(path):
-#     for csv in os.listdir(path):
-#         if os.path.isfile(os.path.join(path, csv)):
-#             yield csv
-#
-# for csv in get_csv(r'C:\Users\andre\OneDrive\Desktop\group-project\Graphdata'):
-# 	csv_files.append(csv)
-#
-# df_csv_append = pd.DataFrame()
-# print(csv_files[0])
-#
-# for file in csv_files:
-# 	df = pd.read_csv(r'C:\Users\andre\OneDrive\Desktop\group-project\Graphdata\\'+file)
-# 	df_csv_append = df_csv_append.append(df)
-#
-# print(df_csv_append.set_index('station'))
-
-path=r'C:\Users\andre\OneDrive\Desktop\group-project\China_shapefiles\china_country.shp'
-geo_china = gpd.read_file(path)
-geo_china.plot()
-plt.xlabel('geo-x')
-plt.title('Pollution in China over Time')
+init_yr = 2013
+fig, ax = plt.subplots()
+ds = xr.open_dataset(f'data/PM_2.5/CHAP_PM2.5_Y1K_{init_yr}_V4.nc')
+df = ds.to_dataframe()
+df = df.reset_index(drop=False).dropna()
+df.plot.hexbin(x='lon', y='lat', ax=ax, C='PM2.5', gridsize=(120,120), cmap='plasma',
+               title=f'PM 2.5 Pollution in China, {init_yr}',
+               xlabel='Longitude', ylabel='Latitude')
+plt.subplots_adjust(left=0.25, bottom=0.25)
+yr_ax = fig.add_axes([0.17, 0.1, 0.65, 0.03])
+yr_slider = Slider(ax=yr_ax, label='Year',valmin=2013, valmax=2021, valinit=2013, valstep=1)
 plt.show()
-
